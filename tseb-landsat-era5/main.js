7f64ccf1ee129cf884796aac84fe3c064204b24a
Oliver Lopez <lopezv.oliver@gmail.com>
2023-03-27 15:09:31 +0300

    Added js extension

--- tseb-landsat-era5/main.js
+++ tseb-landsat-era5/main.js
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Model *
 *******************************************************************************/
-var model = require('users/oliverlopez/mewa-apps:tseb-landsat-era5/model');
+var model = require('users/oliverlopez/mewa-apps:tseb-landsat-era5/model.js');
 var m = model.m;
 
 /*******************************************************************************
  * Components *
 *******************************************************************************/
-var components = require('users/oliverlopez/mewa-apps:tseb-landsat-era5/components');
+var components = require('users/oliverlopez/mewa-apps:tseb-landsat-era5/components.js');
 var c =components.makec(m);
